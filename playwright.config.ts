ba185a8c0c545001a9366658a7ebef2c9545e651
meghaNatraj <megha17194@gmail.com>
2025-03-02 11:24:47 -0500

    Update test to loop through series data

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from '@playwright/test';
 require('custom-env').env(true)
 
 export default defineConfig({
-  use: {
-    baseURL: process.env.BASE_URL, // Set API base URL
-    extraHTTPHeaders: {
-      'Content-Type': 'application/json',
+    testDir: './tests', // Folder containing your API tests
+    workers: process.env.CI ? 2 : 4,
+    use: {
+        baseURL: process.env.BASE_URL, // Set API base URL
+        extraHTTPHeaders: {
+            'Content-Type': 'application/json',
+        }
     },
-  },
+    reporter: 'html',
+    fullyParallel: true
 });
--- tests/seriesObservationsTests.spec.ts
+++ tests/seriesObservationsTests.spec.ts
@@ -1,40 +1,44 @@
 import { test, expect } from '@playwright/test';
 import getSeriesObservations from '../src/apis/getSeriesObservationApi.ts';
 import { getAverage } from '../src/utils/common.ts';
+import inputData from '../test-data/input.json';
 
-test.describe.parallel('Series Observations Tests', () => {
-    test('Verify Series Observations API 200 Success response for valid input @series1', async ({ }) => {
-        const seriesNames = 'FXUSDCAD'; // Mandatory parameter
-        const format = 'json'; // Mandatory parameter
-        const options = {}; // Optional parameters
-        let observationsResponse = await getSeriesObservations(seriesNames, format, options);
+let seriesData = inputData.seriesData; // Get series data from input.json file
+test.describe('Series Observations Tests', () => {
+    seriesData.forEach(({ seriesName, label, description }) => { // Loop through series data
+        test(`Verify Series Observations API 200 Success response for valid input | Series name : ${seriesName} [@series1]`, async ({ }) => {
+            const seriesNames = seriesName; // Mandatory parameter
+            const format = 'json'; // Mandatory parameter
+            const options = {}; // Optional parameters
+            let observationsResponse = await getSeriesObservations(seriesName, format, options);
 
-        //Assertions for response status
-        expect.soft(observationsResponse.responseStatus).toBe(200);
+            //Assertions for response status
+            expect.soft(observationsResponse.responseStatus).toBe(200);
 
-        // Assertions for first level keys
-        expect.soft(observationsResponse.response).not.toBeNull();
-        expect.soft(observationsResponse.response.terms).not.toBeNull();
-        expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
-        expect.soft(observationsResponse.response.observations).not.toBeNull();
+            // Assertions for first level keys
+            expect.soft(observationsResponse.response).not.toBeNull();
+            expect.soft(observationsResponse.response.terms).not.toBeNull();
+            expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
+            expect.soft(observationsResponse.response.observations).not.toBeNull();
 
-        //Assertions for terms object
-        expect.soft(observationsResponse.response.terms.url).toBe('https://www.bankofcanada.ca/terms/');
+            //Assertions for terms object
+            expect.soft(observationsResponse.response.terms.url).toBe('https://www.bankofcanada.ca/terms/');
 
-        //Assertions for seriesDetails object
-        expect.soft(observationsResponse.response.seriesDetail[seriesNames].label).toBe('USD/CAD');
-        expect.soft(observationsResponse.response.seriesDetail[seriesNames].description).toBe('US dollar to Canadian dollar daily exchange rate');
+            //Assertions for seriesDetails object
+            expect.soft(observationsResponse.response.seriesDetail[seriesNames].label).toBe(label);
+            expect.soft(observationsResponse.response.seriesDetail[seriesNames].description).toBe(description);
 
-        // Assertions for Observations array
-        expect.soft(Array.isArray(observationsResponse.response.observations)).toBeTruthy();
-        observationsResponse.response.observations.forEach(element => {
-            expect.soft(element).toHaveProperty(seriesNames);
-            expect.soft(parseFloat(element[seriesNames].v)).toBeGreaterThan(0);
-            expect.soft(parseFloat(element.d)).not.toBeNull();
+            // Assertions for Observations array
+            expect.soft(Array.isArray(observationsResponse.response.observations)).toBeTruthy();
+            observationsResponse.response.observations.forEach(element => {
+                expect.soft(element).toHaveProperty(seriesNames);
+                expect.soft(parseFloat(element[seriesNames].v)).toBeGreaterThan(0);
+                expect.soft(parseFloat(element.d)).not.toBeNull();
+            });
         });
     });
 
-    test('Verify Series Observations API for last 10 weeks and calculate average @series2', async ({ }) => {
+    test('Verify Series Observations API for last 10 weeks and calculate average [@series2]', async ({ }) => {
         const seriesNames = 'FXUSDCAD'; // Mandatory parameter
         const format = 'json'; // Mandatory parameter
         const options = {
@@ -52,7 +56,7 @@ test.describe.parallel('Series Observations Tests', () => {
         // Assertions for Observations array
         let observations = observationsResponse.response.observations;
         expect.soft(Array.isArray(observations)).toBeTruthy();
-        
+
         // Get today's date and 10 weeks ago date
         const today = new Date();
         const tenWeeksAgo = new Date();
