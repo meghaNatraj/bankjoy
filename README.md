caeed6fee8a5b9383295bb0cc02c42d5ed9b7b88
meghaNatraj <megha17194@gmail.com>
2025-03-02 12:39:29 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
 # Bankjoy Technical Assignment
 
 This Repository contains API tests for Bank of Canada Valet APIs
+
+## Prerequisites
+
+### 1. Install Node.js  
+Ensure that **Node.js (LTS version 18 or later)** is installed on your machine. You can check your Node.js version with:  
+
+```bash 
+node -v
+```
+If not installed, download it from [Node.js Official Website](https://nodejs.org/en).
+
+### 2. Install Playwright Test
+
+```bash
+npm install @playwright/test
+```
+
+### 3. Install Typescript
+
+```bash
+npm install typescript --save-dev
+```
+
+### 4. Install Playwright dependencies
+
+```bash
+npx playwright install
+```
+
+## Execution command
+
+```bash
+NODE_ENV=dev npx playwright test
+```
+
+#### Tags
+Tags can be used in execution command to execute selected set of tests or single test. Tags will be added in the test description under spec files
+
+```bash
+NODE_ENV=dev npx playwright test --grep @series1
+```
+
+#### Test Report
+
+Playwright provides different types are [reporters](https://playwright.dev/docs/test-reporters) which can added in playwright.config.ts. Currently HTML Report included in this Framework which can be accessed by below command after each execution.
+
+```bash
+npx playwright show-report
+```
+
+## Folder Structure 
+- **src/**: Contains all API calls, HTTP methods and utilities files.
+  - **apis/**: Holds API calls along with url and other data required
+  - **core/**: Holds core HTTP method calls
+  - **utils/**: Holds any utility scripts or helper functions used in tests.
+- **test-data**/: Contains test data used in tests
+- **tests/**: Test spec files, contains all Tests
+- **.env.dev**/: Contains all data specific to the environment
+- **.gitignore**: Specifies files and folders to ignore in version control (e.g., `node_modules`)
+- **package.json**: Contains project metadata, dependencies, and scripts.
+- **playwright.config.ts**/: Contains all global configurations
+
+## Parallelism     
+
+Tests will be executed in parallel by default and can be changed to "serial" mode for specific spec file, if needed.
+
+## Environment Files
+Currently .env.dev added to organize all environment specific data, more files can be added based on environments e.g, .env.stg and should be passed in execution command to choose different environment
+
+```bash
+NODE_ENV=stg npx playwright test
+```
+
+
+
+
+
+
--- tests/seriesObservationsTests.spec.ts
+++ tests/seriesObservationsTests.spec.ts
@@ -12,28 +12,33 @@ test.describe('Series Observations Tests', () => {
             const options = {}; // Optional parameters
             let observationsResponse = await getSeriesObservations(seriesName, format, options);
 
-            //Assertions for response status
-            expect.soft(observationsResponse.responseStatus).toBe(200);
+            await test.step("Assertions for response status", async () => {
+                expect.soft(observationsResponse.responseStatus).toBe(200);
+            });
 
-            // Assertions for first level keys
-            expect.soft(observationsResponse.response).not.toBeNull();
-            expect.soft(observationsResponse.response.terms).not.toBeNull();
-            expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
-            expect.soft(observationsResponse.response.observations).not.toBeNull();
+            await test.step("Assertions for first level keys", async () => {
+                expect.soft(observationsResponse.response).not.toBeNull();
+                expect.soft(observationsResponse.response.terms).not.toBeNull();
+                expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
+                expect.soft(observationsResponse.response.observations).not.toBeNull();
+            });
 
-            //Assertions for terms object
-            expect.soft(observationsResponse.response.terms.url).toBe('https://www.bankofcanada.ca/terms/');
+            await test.step("Assertions for terms object", async () => {
+                expect.soft(observationsResponse.response.terms.url).toBe('https://www.bankofcanada.ca/terms/');
+            });
 
-            //Assertions for seriesDetails object
-            expect.soft(observationsResponse.response.seriesDetail[seriesNames].label).toBe(label);
-            expect.soft(observationsResponse.response.seriesDetail[seriesNames].description).toBe(description);
+            await test.step("Assertions for seriesDetails object", async () => {
+                expect.soft(observationsResponse.response.seriesDetail[seriesNames].label).toBe(label);
+                expect.soft(observationsResponse.response.seriesDetail[seriesNames].description).toBe(description);
+            });
 
-            // Assertions for Observations array
-            expect.soft(Array.isArray(observationsResponse.response.observations)).toBeTruthy();
-            observationsResponse.response.observations.forEach(element => {
-                expect.soft(element).toHaveProperty(seriesNames);
-                expect.soft(parseFloat(element[seriesNames].v)).toBeGreaterThan(0);
-                expect.soft(parseFloat(element.d)).not.toBeNull();
+            await test.step("Assertions for Observations array", async () => {
+                expect.soft(Array.isArray(observationsResponse.response.observations)).toBeTruthy();
+                observationsResponse.response.observations.forEach(element => {
+                    expect.soft(element).toHaveProperty(seriesNames);
+                    expect.soft(parseFloat(element[seriesNames].v)).toBeGreaterThan(0);
+                    expect.soft(parseFloat(element.d)).not.toBeNull();
+                });
             });
         });
     });
@@ -47,39 +52,44 @@ test.describe('Series Observations Tests', () => {
         let observationsResponse = await getSeriesObservations(seriesNames, format, options);
 
         //Assertions for response status and response body
-        expect.soft(observationsResponse.responseStatus).toBe(200);
-        expect.soft(observationsResponse.response).not.toBeNull();
-        expect.soft(observationsResponse.response.terms).not.toBeNull();
-        expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
-        expect.soft(observationsResponse.response.observations).not.toBeNull();
-
-        // Assertions for Observations array
-        let observations = observationsResponse.response.observations;
-        expect.soft(Array.isArray(observations)).toBeTruthy();
-
-        // Get today's date and 10 weeks ago date
-        const today = new Date();
-        const tenWeeksAgo = new Date();
-        tenWeeksAgo.setDate(today.getDate() - 10 * 7); // Subtract 10 weeks
-
-        // Assert each observation date is within the last 10 weeks
-        observations.forEach(observation => {
-            const observationDate = new Date(observation.d);
-            expect.soft(observationDate.getTime()).toBeGreaterThan(tenWeeksAgo.getTime());
-            expect.soft(parseFloat(observation[seriesNames].v)).toBeGreaterThan(0);
+        await test.step("Assertions for response status and response body", async () => {
+            expect.soft(observationsResponse.responseStatus).toBe(200);
+            expect.soft(observationsResponse.response).not.toBeNull();
+            expect.soft(observationsResponse.response.terms).not.toBeNull();
+            expect.soft(observationsResponse.response.seriesDetail).not.toBeNull();
+            expect.soft(observationsResponse.response.observations).not.toBeNull();
         });
 
-        // Extract rates from observations
-        let extractedRates = observations.map(observation => parseFloat(observation[seriesNames].v));
+        let observations = observationsResponse.response.observations;
+        await test.step("Assertions for Observations array", async () => {
+            expect.soft(Array.isArray(observations)).toBeTruthy();
 
-        // calculate average rate
-        let averageRate = getAverage(extractedRates);
+            // Get today's date and 10 weeks ago date
+            const today = new Date();
+            const tenWeeksAgo = new Date();
+            tenWeeksAgo.setDate(today.getDate() - 10 * 7); // Subtract 10 weeks
 
-        // Assert average rate is greater than 0
-        console.log("Average USD to CAD average exchange rate in last 10 weeks: ", averageRate);
-        expect.soft(averageRate).toBeGreaterThan(0);
+            // Assert each observation date is within the last 10 weeks
+            observations.forEach(observation => {
+                const observationDate = new Date(observation.d);
+                expect.soft(observationDate.getTime()).toBeGreaterThan(tenWeeksAgo.getTime());
+                expect.soft(parseFloat(observation[seriesNames].v)).toBeGreaterThan(0);
+            });
+        });
+
+        await test.step("Calculate average exchange rate", async () => {
+            // Extract rates from observations
+            let extractedRates = observations.map(observation => parseFloat(observation[seriesNames].v));
+
+            // calculate average rate
+            let averageRate = getAverage(extractedRates);
+
+            // Assert average rate is greater than 0
+            console.log("Average USD to CAD average exchange rate in last 10 weeks: ", averageRate);
+            expect.soft(averageRate).toBeGreaterThan(0);
+        });
     });
-    
+
     // Negative test case
     test('Verify Series Observations API 404 response for invalid series name [@series3]', async ({ }) => {
         const seriesName = 'invalidSeriesName'; // Invalid series name
@@ -89,17 +99,17 @@ test.describe('Series Observations Tests', () => {
 
         //Assertions for response status
         expect.soft(observationsResponse.responseStatus).toBe(404);
-        expect.soft(observationsResponse.response.message).toBe(`Series ${seriesName} not found.`);   
+        expect.soft(observationsResponse.response.message).toBe(`Series ${seriesName} not found.`);
     });
 
     test('Verify Series Observations API 404 response for missing series name [@series4]', async ({ }) => {
         const format = 'json'; // Mandatory parameter
         const options = {}; // Optional parameters
-        let observationsResponse = await getSeriesObservations('', format, options);        
+        let observationsResponse = await getSeriesObservations('', format, options);
 
         //Assertions for response status
         expect.soft(observationsResponse.responseStatus).toBe(404);
-        expect.soft(observationsResponse.response.message).toBe('Series json not found.');   
+        expect.soft(observationsResponse.response.message).toBe('Series json not found.');
     });
 
     test('Verify Series Observations API 400 response for invalid format [@series5]', async ({ }) => {
@@ -110,7 +120,7 @@ test.describe('Series Observations Tests', () => {
 
         //Assertions for response status
         expect.soft(observationsResponse.responseStatus).toBe(400);
-        expect.soft(observationsResponse.response.message).toBe('Bad output format (invalidformat) requested.');   
+        expect.soft(observationsResponse.response.message).toBe('Bad output format (invalidformat) requested.');
     });
 
     test('Verify Series Observations API 400 response for invalid optional parameter [@series6]', async ({ }) => {
@@ -123,7 +133,7 @@ test.describe('Series Observations Tests', () => {
 
         //Assertions for response status
         expect.soft(observationsResponse.responseStatus).toBe(400);
-        expect.soft(observationsResponse.response.message).toBe('The following query parameters are invalid: invalidParam');   
+        expect.soft(observationsResponse.response.message).toBe('The following query parameters are invalid: invalidParam');
     });
 
     test('Verify Series Observations API 400 response for invalid optional parameter value [@series7]', async ({ }) => {
